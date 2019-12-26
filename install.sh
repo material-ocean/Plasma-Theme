9c8bd212ac1e43840d490c71a660037df2297fca
blacksuan19 <abubakaryagob@gmail.com>
2019-12-26 11:44:45 +0800

    install: fix copy destination
    Signed-off-by: blacksuan19 <abubakaryagob@gmail.com>

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env sh
 echo "installing theme..."
-cp -r "Material Ocean" ~/.local/share/plasma/desktoptheme
+cp -r "Material Ocean" ~/.local/share/plasma/desktoptheme/Material\ Ocean
 echo "installing colorscheme..."
 cp MaterialOcean.colors ~/.local/share/color-schemes
 printf "Installation Successful!\nGo to settings and select the desktop theme and
