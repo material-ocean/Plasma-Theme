30725b8b3ddb14c5d1e8c977d0235b572b2a573e
prateekpunetha <hi@prateekpunetha.dev>
2023-06-03 18:08:04 +0530

    refactor directory creation and fix file path
    - Ensure the directory exists in Plasma by default.
    - Update the command to "cp MaterialOcean.colors ~/.local/share/color-schemes/" to correctly create a directory named "color-schemes" instead of a file.
    
    Signed-off-by: prateekpunetha <hi@prateekpunetha.dev>

--- install.sh
+++ install.sh
@@ -1,7 +1,9 @@
 #!/usr/bin/env sh
 echo "installing theme..."
+mkdir -p ~/.local/share/plasma/desktoptheme/
 cp -r Material-Ocean ~/.local/share/plasma/desktoptheme/
 echo "installing colorscheme..."
-cp MaterialOcean.colors ~/.local/share/color-schemes
+mkdir -p ~/.local/share/color-schemes/
+cp MaterialOcean.colors ~/.local/share/color-schemes/
 printf "Installation Successful!\nGo to settings and select the desktop theme and
 color scheme"
